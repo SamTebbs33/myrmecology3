7be086ae00947a1e9f04c363136754d8b94ea831
SamTebbs33 <samuel.tebbs@yahoo.com>
2016-07-30 18:09:54 +0100

    Removed BlockAntHill.addSpecies and changed constructor argument to varargs

--- src/main/scala/com/github/samtebbs33/block/BlockAntHill.scala
+++ src/main/scala/com/github/samtebbs33/block/BlockAntHill.scala
@@ -6,8 +6,6 @@ import net.minecraft.block.material.Material
 /**
   * Created by samtebbs on 30/07/2016.
   */
-class BlockAntHill(name : String, species : List[AntSpecies]) extends MyrmecologyBlock(Material.CACTUS, name) {
-
-  def addSpecies(s : AntSpecies) = s :: species
+class BlockAntHill(name : String, species : AntSpecies*) extends MyrmecologyBlock(Material.CACTUS, name) {
 
 }
