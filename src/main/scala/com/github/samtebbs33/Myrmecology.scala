5bfaba5316fa663bb0b6ac9e8e83614ca9c73e73
SamTebbs33 <samuel.tebbs@yahoo.com>
2016-07-30 09:24:32 +0100

    Initial commit

---
+++ src/main/scala/com/github/samtebbs33/Myrmecology.scala
@@ -0,0 +1,22 @@
+package com.github.samtebbs33
+
+import net.minecraftforge.fml.common.Mod
+import net.minecraftforge.fml.common.Mod.EventHandler
+import net.minecraftforge.fml.common.event.FMLInitializationEvent
+
+/**
+  * Created by samtebbs on 30/07/2016.
+  */
+@Mod(modid = Myrmecology.MOD_ID, modLanguage = "scala", version = Myrmecology.MOD_VERSION, name = Myrmecology.MOD_NAME)
+object Myrmecology {
+
+  final val MOD_ID = "Mymecology"
+  final val MOD_VERSION = "0.0.0"
+  final val MOD_NAME = MOD_ID
+
+  @EventHandler
+  def init(e : FMLInitializationEvent): Unit = {
+    println("myrmecology init");
+  }
+
+}
