046fb3ccf38516cb20b38fefa13d98950814a424
SamTebbs33 <samuel.tebbs@yahoo.com>
2016-08-05 21:49:08 +0100

    Revert "Merged dev into f-solarium-block"
    This reverts commit e90de72e396ce2662a1e5c677c447e88cb3bc431.

--- src/main/scala/registry/Registry.scala
+++ src/main/scala/registry/Registry.scala
@@ -11,7 +11,7 @@ import net.minecraftforge.fml.relauncher.{Side, SideOnly}
 	*/
 object Registry {
 
-	final val creativeTab = makeCreativeTab(ItemRegistry.antPlains)
+	final val creativeTab = makeCreativeTab(Items.ARROW)
 
 	val PLAINS = "plains"
 
