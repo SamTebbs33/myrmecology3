94eacca774c2913f7a7697b025b7f6f2228fe77c
SamTebbs33 <samuel.tebbs@yahoo.com>
2016-08-05 22:05:57 +0100

    Fixed problem with creative tab being created and assigned before item creation

--- src/main/scala/com/github/samtebbs33/Proxy.scala
+++ src/main/scala/com/github/samtebbs33/Proxy.scala
@@ -2,7 +2,7 @@ package com.github.samtebbs33
 
 import net.minecraft.item.Item
 import net.minecraftforge.fml.common.event.{FMLInitializationEvent, FMLPostInitializationEvent, FMLPreInitializationEvent}
-import registry.{BlockRegistry, ItemRegistry}
+import registry.{BlockRegistry, ItemRegistry, Registry}
 
 /**
 	* Created by samtebbs on 31/07/2016.
@@ -12,8 +12,9 @@ class Proxy {
 	def registerModel(item: Item) {}
 
 	def init(e: FMLInitializationEvent): Unit = {
-		BlockRegistry.registerBlocks()
-		ItemRegistry.registerItems()
+		BlockRegistry.registerBlocks
+		ItemRegistry.registerItems
+		Registry.registerCreativeTab
 	}
 
 	def preInit(e: FMLPreInitializationEvent): Unit = {}
