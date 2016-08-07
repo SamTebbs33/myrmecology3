2422f886a3d5549ce14d4ba08fe8e12a2d11f130
SamTebbs33 <samuel.tebbs@yahoo.com>
2016-08-07 11:19:53 +0100

    Worked on update() in TileEntitySolarium

--- src/main/scala/com/github/samtebbs33/common/tileentity/TileEntitySolarium.scala
+++ src/main/scala/com/github/samtebbs33/common/tileentity/TileEntitySolarium.scala
@@ -2,13 +2,15 @@ package com.github.samtebbs33.common.tileentity
 
 import com.github.samtebbs33.common.ant.AntTypes
 import com.github.samtebbs33.common.item.ItemAnt
-import com.github.samtebbs33.registry.BlockRegistry
+import com.github.samtebbs33.registry.{AntTraitRegistry, BlockRegistry, ItemRegistry}
 import net.minecraft.entity.player.EntityPlayer
 import net.minecraft.inventory.IInventory
 import net.minecraft.item.ItemStack
 import net.minecraft.tileentity.TileEntity
 import net.minecraft.util.ITickable
 import net.minecraftforge.event.entity.minecart.MinecartUpdateEvent
+
+import scala.util.Random
 
 /**
 	* Created by samtebbs on 05/08/2016.
@@ -19,6 +21,7 @@ class TileEntitySolarium extends MyrmecologyTileEntityContainer(BlockRegistry.NA
 	val TICKS_PER_SECOND = 20
 	var progress = 0
 	var ticks = 0
+	var product : Option[ItemStack] = None
 
 	def updateProgress() = {
 		//println("progress update")
@@ -29,16 +32,33 @@ class TileEntitySolarium extends MyrmecologyTileEntityContainer(BlockRegistry.NA
 		}
 	}
 
+	def reset(): Unit = {
+		ticks = 0
+		progress = 0
+		product = None
+	}
+
 	override def update(): Unit = {
 		val larva = getStackInSlot(SLOT_LARVA)
 		if(larva != null) {
+			val species = larva.getItem.asInstanceOf[ItemAnt].species
+			if(product.isEmpty) product = Some(new ItemStack(ItemRegistry.getAnt(species).get, 1, Random.nextInt(AntTypes.values.size)))
 			updateProgress()
-		}
+			if(progress >= species.traits.getTrait(AntTraitRegistry.incubationTime)) {
+				println("Finishing incubation")
+				decrStackSize(SLOT_LARVA, 1)
+				addStack(product.get)
+				reset()
+			}
+		} else reset()
 	}
 
 	override def getInventoryStackLimit: Int = 64
 
-	override def isItemValidForSlot(index: Int, stack: ItemStack): Boolean = index == SLOT_LARVA && stack.getItem.isInstanceOf[ItemAnt] && stack.getMetadata == AntTypes.LARVA.id
+	override def isItemValidForSlot(index: Int, stack: ItemStack): Boolean = index match {
+		case SLOT_LARVA => stack.getItem.isInstanceOf[ItemAnt] && stack.getMetadata == AntTypes.LARVA.id
+		case _ => stack.getItem.isInstanceOf[ItemAnt]
+	}
 
 	override def openInventory(player: EntityPlayer): Unit = ???
 
