6e7829d83b6193ea0add2447094a1c1ff182c4d4
SamTebbs33 <samuel.tebbs@yahoo.com>
2016-08-07 22:46:33 +0100

    Added breeding chamber tile entity, block and GUI ID

--- src/main/scala/com/github/samtebbs33/common/GuiHandler.scala
+++ src/main/scala/com/github/samtebbs33/common/GuiHandler.scala
@@ -14,6 +14,7 @@ import net.minecraftforge.fml.common.network.IGuiHandler
 object GuiHandler extends IGuiHandler {
 
 	val ID_SOLARIUM = 0
+	val ID_BREEDING_CHAMBER = 1
 
 	def getTileEntity[T <: MyrmecologyTileEntityContainer](world: World, x: Int, y: Int, z: Int, clazz: Class[T]): T = clazz.cast(world.getTileEntity(new BlockPos(x, y, z)))
 
