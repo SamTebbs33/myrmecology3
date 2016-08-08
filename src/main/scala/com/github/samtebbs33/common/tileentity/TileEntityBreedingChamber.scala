8cb2255b36aad9e6930feffd261f685d30766f55
SamTebbs33 <samuel.tebbs@yahoo.com>
2016-08-08 20:52:59 +0100

    Fleshed out breeding chamber tile entity

--- src/main/scala/com/github/samtebbs33/common/tileentity/TileEntityBreedingChamber.scala
+++ src/main/scala/com/github/samtebbs33/common/tileentity/TileEntityBreedingChamber.scala
@@ -1,22 +1,47 @@
 package com.github.samtebbs33.common.tileentity
 
+import com.github.samtebbs33.common.ant.AntTypes
+import com.github.samtebbs33.common.item.ItemAnt
 import com.github.samtebbs33.registry.BlockRegistry
 import net.minecraft.entity.player.EntityPlayer
 import net.minecraft.item.ItemStack
+import net.minecraft.util.ITickable
 
 /**
 	* Created by samtebbs on 07/08/2016.
 	*/
-class TileEntityBreedingChamber extends MyrmecologyTileEntityContainer(BlockRegistry.NAME_BREEDING_CHAMBER, 17) {
-	override def getInventoryStackLimit: Int = ???
+class TileEntityBreedingChamber extends MyrmecologyTileEntityContainer(BlockRegistry.NAME_BREEDING_CHAMBER, 17) with ITickable {
 
-	override def isItemValidForSlot(index: Int, stack: ItemStack): Boolean = ???
+	val SLOT_DRONE = 1
+	val SLOT_QUEEN = 0
 
-	override def openInventory(player: EntityPlayer): Unit = ???
+	var progress = 0
+	var ticks = 0
+	var targetTime = 0
 
-	override def getFieldCount: Int = ???
+	override def getInventoryStackLimit: Int = 64
 
-	override def getField(id: Int): Int = ???
+	override def isItemValidForSlot(index: Int, stack: ItemStack): Boolean = stack.getItem.isInstanceOf[ItemAnt] && (index match {
+		case SLOT_DRONE => stack.getMetadata == AntTypes.DRONE.id
+		case SLOT_QUEEN => stack.getMetadata == AntTypes.QUEEN.id
+		case _ => true
+	})
 
-	override def setField(id: Int, value: Int): Unit = ???
+	override def openInventory(player: EntityPlayer): Unit = ()
+
+	override def getFieldCount: Int = 2
+
+	override def getField(id: Int): Int = id match {
+		case 0 => progress
+		case _ => targetTime
+	}
+
+	override def setField(id: Int, value: Int): Unit = id match {
+		case 0 => progress = value
+		case 1 => targetTime = value
+	}
+
+	override def update(): Unit = {
+		// TODO
+	}
 }
