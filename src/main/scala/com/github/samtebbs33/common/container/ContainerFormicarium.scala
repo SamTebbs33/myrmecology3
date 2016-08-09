cc7cfab1a5121965f3c73d3af07e0f981313b7af
SamTebbs33 <samuel.tebbs@yahoo.com>
2016-08-09 21:53:22 +0100

    Fleshed out formicarium container and tile entity

--- src/main/scala/com/github/samtebbs33/common/container/ContainerFormicarium.scala
+++ src/main/scala/com/github/samtebbs33/common/container/ContainerFormicarium.scala
@@ -9,5 +9,17 @@ import net.minecraft.inventory.{IInventory, Slot}
   * Created by samtebbs on 09/08/2016.
   */
 class ContainerFormicarium(playerInv: IInventory, te: TileEntityFormicarium) extends MyrmecologyContainer(playerInv, te) {
-  override def slots: util.List[Slot] = ???
+  override def slots: util.List[Slot] = {
+    val list = new util.LinkedList[Slot]
+    def addSlotGroup(baseX : Int, baseY : Int, baseID: Int): Unit = {
+      var id = baseID
+      Range(0, 3).foreach(y ⇒ Range(0, 3).foreach(x ⇒ {
+        list.add(new Slot(te, id, baseX + 18 * x, baseY + 18 * y))
+        id += 1
+      }))
+    }
+    addSlotGroup(18, 17, 0)
+    addSlotGroup(99, 17, 9)
+    list
+  }
 }
--- src/main/scala/com/github/samtebbs33/common/tileentity/TileEntityFormicarium.scala
+++ src/main/scala/com/github/samtebbs33/common/tileentity/TileEntityFormicarium.scala
@@ -1,5 +1,7 @@
 package com.github.samtebbs33.common.tileentity
 
+import com.github.samtebbs33.common.ant.AntTypes
+import com.github.samtebbs33.common.item.ItemAnt
 import com.github.samtebbs33.registry.BlockRegistry
 import net.minecraft.entity.player.EntityPlayer
 import net.minecraft.item.ItemStack
@@ -7,16 +9,22 @@ import net.minecraft.item.ItemStack
 /**
   * Created by samtebbs on 09/08/2016.
   */
-class TileEntityFormicarium extends MyrmecologyTileEntityContainer(BlockRegistry.NAME_FORMICARIUM, 20) {
-  override def getInventoryStackLimit: Int = ???
+class TileEntityFormicarium extends MyrmecologyTileEntityContainer(BlockRegistry.NAME_FORMICARIUM, 18) {
 
-  override def isItemValidForSlot(index: Int, stack: ItemStack): Boolean = ???
+  val SLOT_ANT_END = 9
 
-  override def openInventory(player: EntityPlayer): Unit = ???
+  override def getInventoryStackLimit: Int = 64
 
-  override def getFieldCount: Int = ???
+  override def isItemValidForSlot(index: Int, stack: ItemStack): Boolean = index match {
+    case x if x < SLOT_ANT_END ⇒ stack.getItem.isInstanceOf[ItemAnt] && stack.getMetadata == AntTypes.WORKER.id
+    case _ ⇒ true
+  }
 
-  override def getField(id: Int): Int = ???
+  override def openInventory(player: EntityPlayer): Unit = {}
 
-  override def setField(id: Int, value: Int): Unit = ???
+  override def getFieldCount: Int = 0
+
+  override def getField(id: Int): Int = 0
+
+  override def setField(id: Int, value: Int): Unit = 0
 }
