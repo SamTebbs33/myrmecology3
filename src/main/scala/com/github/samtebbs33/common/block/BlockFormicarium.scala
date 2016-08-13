0b80ce410ea9a8d5f6595c38fadfe4c056af6e66
SamTebbs33 <samuel.tebbs@yahoo.com>
2016-08-13 22:42:00 +0100

    Added block recipes

--- src/main/scala/com/github/samtebbs33/common/block/BlockFormicarium.scala
+++ src/main/scala/com/github/samtebbs33/common/block/BlockFormicarium.scala
@@ -1,14 +1,20 @@
 package com.github.samtebbs33.common.block
 
-import com.github.samtebbs33.common.GuiHandler
+import com.github.samtebbs33.common.{CraftingRecipe, GuiHandler}
 import com.github.samtebbs33.common.tileentity.TileEntityFormicarium
 import net.minecraft.block.material.Material
+import net.minecraft.init.{Blocks, Items}
+import net.minecraft.item.ItemStack
 
 /**
   * Created by samtebbs on 09/08/2016.
   */
 class BlockFormicarium(name: String) extends MyrmecologyBlockContainer(Material.GLASS, name, GuiHandler.ID_FORMICARIUM, classOf[TileEntityFormicarium]) {
-
-
-
+  override def craftingRecipe: Option[CraftingRecipe] = Some(new CraftingRecipe(
+    new ItemStack(this), Array("wdw", "gsg", "wdw"), Map(
+      'w' → new ItemStack(Blocks.WOODEN_SLAB),
+      'd' → new ItemStack(Items.DYE, 1, 1),
+      'g' → new ItemStack(Blocks.GLASS),
+      's' → new ItemStack(Blocks.SAND))
+  ))
 }
