a537c0c8d91e46e70936fe6e1e557a285a98d0b5
SamTebbs33 <samuel.tebbs@yahoo.com>
2016-08-18 19:25:59 +0100

    Added ant extractor crafting recipe

--- src/main/scala/com/github/samtebbs33/common/item/ItemAntExtractor.scala
+++ src/main/scala/com/github/samtebbs33/common/item/ItemAntExtractor.scala
@@ -1,7 +1,10 @@
 package com.github.samtebbs33.common.item
 
+import com.github.samtebbs33.common.CraftingRecipe
 import com.github.samtebbs33.common.block.BlockAntHill
 import net.minecraft.block.state.IBlockState
+import net.minecraft.init.Items
+import net.minecraft.item.{ItemDye, ItemStack}
 
 /**
   * Created by samtebbs on 05/08/2016.
@@ -9,5 +12,9 @@ import net.minecraft.block.state.IBlockState
 class ItemAntExtractor(s: String) extends MyrmecologyItem(s) {
 
   override def canHarvestBlock(blockIn: IBlockState): Boolean = blockIn.getBlock.isInstanceOf[BlockAntHill]
+
+  override def craftingRecipe: Option[CraftingRecipe] = Some(new CraftingRecipe(
+    new ItemStack(this), Array("", "dsd", ""), Map('d' → new ItemStack(Items.DYE, 1, 2), 's' → new ItemStack(Items.IRON_SHOVEL))
+  ))
 
 }
