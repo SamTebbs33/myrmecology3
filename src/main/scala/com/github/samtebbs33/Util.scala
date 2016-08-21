0451cc8a67cb4f72ae3090d48feefb5d6fe1231d
SamTebbs33 <samuel.tebbs@yahoo.com>
2016-08-21 15:26:53 +0100

    Added world util class

--- src/main/scala/com/github/samtebbs33/Util.scala
+++ src/main/scala/com/github/samtebbs33/Util.scala
@@ -1,8 +1,16 @@
 package com.github.samtebbs33
 
+import java.util.stream.Collectors
+
+import net.minecraft.block.state.IBlockState
 import net.minecraft.item.ItemStack
+import net.minecraft.util.math.{BlockPos, Vec3i}
+import net.minecraft.world.World
 
 import scala.util.Random
+import scala.collection.JavaConversions._
+import scala.collection.JavaConverters._
+import scala.collection.immutable.HashSet
 
 /**
   * Created by samtebbs on 09/08/2016.
@@ -25,6 +33,25 @@ object Util {
 
   implicit class SeqUtil[T](seq: Seq[T]) {
     def rand = seq(Random.nextInt(seq.size))
+  }
+
+  implicit class WorldUtil(world: World) {
+    def getBlocksInRadius(pos: BlockPos, radius: Vec3i): Set[IBlockState] = {
+      val posX = pos.getX
+      val posY = pos.getY
+      val posZ = pos.getZ
+      val radiusX = radius.getX
+      val radiusY = radius.getY
+      val radiusZ = radius.getZ
+      val set = Set[IBlockState]()
+      Range(posX - radiusX, posX + radiusX).foreach(x ⇒
+        Range(posY - radiusY, posY + radiusY).foreach(y ⇒
+          Range(posZ - radiusZ, posZ + radiusZ).toStream.map(z ⇒ new BlockPos(x, y, z)).filter(!world.isAirBlock(_)).map(world.getBlockState).foreach(set.add)
+        )
+      )
+      set
+    }
+    def getTypedBlocksInRadius[T](pos: BlockPos, radius: Vec3i) = getBlocksInRadius(pos, radius).filter(_.getBlock.isInstanceOf[T])
   }
 
 }
