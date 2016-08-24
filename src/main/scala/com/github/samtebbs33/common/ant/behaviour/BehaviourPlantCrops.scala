dd6c1fa3879f4f1935e54ede4f3dfb654b7016f9
SamTebbs33 <samuel.tebbs@yahoo.com>
2016-08-24 21:29:01 +0100

    Working on crop planting

--- src/main/scala/com/github/samtebbs33/common/ant/behaviour/BehaviourPlantCrops.scala
+++ src/main/scala/com/github/samtebbs33/common/ant/behaviour/BehaviourPlantCrops.scala
@@ -5,6 +5,7 @@ import net.minecraft.block.BlockCrops
 import net.minecraft.init.{Blocks, Items}
 import net.minecraft.item.{ItemSeeds, ItemStack}
 import net.minecraft.util.math.Vec3i
+import net.minecraftforge.oredict.OreDictionary
 
 /**
   * Created by samuelt on 24/08/2016.
@@ -14,9 +15,8 @@ class BehaviourPlantCrops(name: String) extends Behaviour(name) {
   val radius = new Vec3i(5, 5, 5)
 
   override def execute(formicarium: TileEntityFormicarium, numAnts: Int, stacks: Seq[ItemStack]): Unit = {
-   val world = formicarium.getWorld
-    val crops = world.getTypedBlocksInRadius[BlockCrops](formicarium.getPos, radius)
-    val plantableItems = crops.map(pair => pair._2.getBlock.asInstanceOf[BlockCrops].getItem(world, pair._1, pair._2))
+    val world = formicarium.getWorld
+    val plantableBlocks = OreDictionary.get
   }
 
 }
