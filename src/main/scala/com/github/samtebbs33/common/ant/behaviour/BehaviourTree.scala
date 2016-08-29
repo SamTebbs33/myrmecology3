6703a1373d551f5035321b7933e232337b187ee3
SamTebbs33 <samuel.tebbs@yahoo.com>
2016-08-29 11:05:25 +0100

    Added randomBehaviour and allBehaviour to BehaviourTree

--- src/main/scala/com/github/samtebbs33/common/ant/behaviour/BehaviourTree.scala
+++ src/main/scala/com/github/samtebbs33/common/ant/behaviour/BehaviourTree.scala
@@ -1,11 +1,17 @@
 package com.github.samtebbs33.common.ant.behaviour
 
 import java.util
+import java.util.stream.Collectors
+
+import scala.collection.mutable
+import scala.util.Random
+import scala.collection.JavaConverters._
+import scala.collection.JavaConversions._
 
 /**
   * Created by samtebbs on 20/08/2016.
   */
-class BehaviourTree(name: String) {
+class BehaviourTree(val name: String) {
 
   val children = new util.ArrayList[BehaviourTree]()
   val behaviour = new util.ArrayList[Behaviour]()
@@ -18,6 +24,17 @@ class BehaviourTree(name: String) {
   def addBehaviour(antAI: Behaviour) = {
     behaviour.add(antAI)
     antAI
+  }
+
+  def allBehaviour: util.List[Behaviour] = {
+    val list = behaviour.stream().collect(Collectors.toList[Behaviour])
+    children.foreach(_.allBehaviour.foreach(list.add))
+    list
+  }
+
+  def randomBehaviour: Behaviour = {
+    val all = allBehaviour
+    all.get(Random.nextInt(all.size))
   }
 
 }
