0371f998fdf59ec9f7edacc130ea2f9971839307
SamTebbs33 <samuel.tebbs@yahoo.com>
2016-09-15 22:07:48 +0100

    Fixed missing procedure style declarations

--- src/main/scala/com/github/samtebbs33/common/ant/behaviour/Behaviour.scala
+++ src/main/scala/com/github/samtebbs33/common/ant/behaviour/Behaviour.scala
@@ -12,7 +12,7 @@ abstract class Behaviour(val name: String) {
 
   Behaviour.behaviourMap.put(name, this)
 
-  def execute(formicarium: TileEntityFormicarium, numAnts: Int, stacks: Seq[ItemStack])
+  def execute(formicarium: TileEntityFormicarium, numAnts: Int, stacks: Seq[ItemStack]): Unit
 
 }
 
